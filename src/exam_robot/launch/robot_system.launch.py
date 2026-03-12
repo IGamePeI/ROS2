24c8c94067e0565acb74501d687e173fc55eff11
IGamePeI <rahmatulinamir5@gmail.com>
2026-03-12 16:03:02 +0000

    exam_robot

---
+++ src/exam_robot/launch/robot_system.launch.py
@@ -0,0 +1,32 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='exam_robot',
+            executable='battery_node',
+            name='battery_node',
+            output='screen'
+        ),
+        Node(
+            package='exam_robot',
+            executable='distance_sensor',
+            name='distance_sensor',
+            output='screen'
+        ),
+        Node(
+            package='exam_robot',
+            executable='status_display',
+            name='status_display',
+            output='screen'
+        ),
+        Node(
+            package='exam_robot',
+            executable='robot_controller',
+            name='robot_controller',
+            output='screen'
+        ),
+    ])
+

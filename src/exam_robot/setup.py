f8aef755a82277ea6cd0926b5b9596caf2fd5c48
IGamePeI <rahmatulinamir5@gmail.com>
2026-03-12 17:18:29 +0000

    Контрольная изменения

--- src/exam_robot/setup.py
+++ src/exam_robot/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 import os
-from glob import glob
+from glob import glob  # ← Добавлено
 
 package_name = 'exam_robot'
 
@@ -8,32 +8,27 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
+
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), 
-            glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'urdf'), 
-            glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='student',
     maintainer_email='student@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
     entry_points={
         'console_scripts': [
-		    'battery_node = exam_robot.battery_node:main',
+            'battery_node = exam_robot.battery_node:main',
             'distance_sensor = exam_robot.distance_sensor:main',
             'robot_controller = exam_robot.robot_controller:main',
             'status_display = exam_robot.status_display:main',
         ],
     },
 )
+
